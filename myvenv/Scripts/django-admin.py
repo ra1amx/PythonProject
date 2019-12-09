15b0ae99b358395091520c6a759b85ddcba0681a
Ivan Eremin <ereminspb@gmail.com>
2019-12-09 21:39:44 +0300

    Initial commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\иван\documents\arhivc\openserver\domains\localhost\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
